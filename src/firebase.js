6339d123873f043407190c7139b4beef298754e9
awantech-io <hello.wanlab@gmail.com>
2020-10-20 23:42:32 +0800

    firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBIOWcVsVMdzBt9QFTsq4nIMRqU8KFWdoo",
+    authDomain: "clone-44c3b.firebaseapp.com",
+    databaseURL: "https://clone-44c3b.firebaseio.com",
+    projectId: "clone-44c3b",
+    storageBucket: "clone-44c3b.appspot.com",
+    messagingSenderId: "212346881458",
+    appId: "1:212346881458:web:af01a0a59bcde742cdd0ab",
+    measurementId: "G-70S274P8ZN"
+
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
